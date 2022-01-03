faa49c462f168f4a5c6d86e3878d5042945f34a4
Caner Kaya <62384189+Ferocious17@users.noreply.github.com>
2022-01-03 09:30:54 +0100

    Add files via upload

---
+++ DocsifySidebarUpdaterV2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DocsifySidebarUpdaterV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Updater updater = new Updater(@"C:\M133\m133_lernportfolio-Ferocious17\docs\Wochen", 16);
+            updater.Update();
+
+            Console.WriteLine("Done.");
+        }
+    }
+}
---
+++ DocsifySidebarUpdaterV2/Updater.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace DocsifySidebarUpdaterV2
+{
+    public class Updater
+    {
+        public string Folderpath { get; set; }
+        public int Week { get; set; }
+
+        public Updater(string folderpath, int week)
+        {
+            Folderpath = folderpath;
+            Week = week;
+        }
+
+        public void Update()
+        {
+            if (!Directory.Exists(Folderpath))
+                throw new DirectoryNotFoundException("Could not find specified directory!");
+
+            if (Folderpath.EndsWith(Path.DirectorySeparatorChar))
+                Folderpath = Folderpath.Substring(0, Folderpath.Length - 1);
+
+            _update(Folderpath);
+        }
+
+        private void _update(string path)
+        {
+            string sidebarPath = path + Path.DirectorySeparatorChar + "_sidebar.md";
+
+            if (File.Exists(sidebarPath))
+            {
+                string[] lines = File.ReadAllLines(sidebarPath);
+                lines[lines.Length - 3] = $"{lines[lines.Length - 3]}{Environment.NewLine}    * [Woche {Week}](Wochen/Woche{Week}/)";
+                File.WriteAllLines(sidebarPath, lines);
+            }
+
+            foreach (var subDir in Directory.GetDirectories(path))
+                _update(subDir);
+        }
+    }
+}
